c5a3f6d612fac66a6d44df073db7f0e7a3fdf2e4
Gakuto1112 <gakuto131112@gmail.com>
2022-02-06 19:55:34 +0900

    実績リストの並べ替え実装

--- SteamAchievementsTracker.js
+++ SteamAchievementsTracker.js
@@ -1,9 +1,39 @@
 const myNameElement = document.getElementById("account_pulldown");
+const achievementsOrderedList = [];
+
+function sortAchievements() {
+	/*実績の並べ替え
+		1. 取得済み実績 -> <br>×3 -> 緑色実績 -> 無色実績 -> 赤色実績 -> 「〇個の秘密の実績が残っている」
+		2. 
+			- 取得済み実績：そのまま
+			- 未取得実績：achievementsOrderedListの順に基づいて並べ替え
+	*/
+	const achievementListElement = document.getElementById("personalAchieve");
+	const achievementList = Array.from(achievementListElement.children);
+	achievementList.sort((firstElement, secondElement) => {
+		if((firstElement.hasAttribute("data-panel") || secondElement.hasAttribute("data-panel")) || (firstElement.tagName == "BR" || secondElement.tagName == "BR")) return 0;
+		else if(firstElement.firstElementChild.classList.contains("achieveHiddenBox") || secondElement.firstElementChild.classList.contains("achieveHiddenBox")) return 1;
+		else {
+			let firstElementValue = achievementsOrderedList.indexOf(firstElement.lastElementChild.firstElementChild.firstElementChild.innerHTML);
+			let secondElementValue = achievementsOrderedList.indexOf(secondElement.lastElementChild.firstElementChild.firstElementChild.innerHTML);
+			if(firstElement.lastElementChild.classList.contains("sat_good_row")) firstElementValue -= achievementListElement.children.length;
+			else if(firstElement.lastElementChild.classList.contains("sat_bad_row")) firstElementValue += achievementListElement.children.length;
+			if(secondElement.lastElementChild.classList.contains("sat_good_row")) secondElementValue -= achievementListElement.children.length;
+			else if(secondElement.lastElementChild.classList.contains("sat_bad_row")) secondElementValue += achievementListElement.children.length;
+			return firstElementValue - secondElementValue;
+		}
+	});
+	while(achievementListElement.firstElementChild) achievementListElement.removeChild(achievementListElement.firstElementChild);
+	achievementList.forEach((achievementRow) => achievementListElement.appendChild(achievementRow));
+}
+
 if(myNameElement) {
 	const personaNameTextClassElements = document.getElementsByClassName("persona_name_text_content");
 	if(personaNameTextClassElements.item(personaNameTextClassElements.length - 1).innerText.slice(0, -1) == myNameElement.innerText) {
-		Array.from(document.getElementById("personalAchieve").children).forEach((element) => {
-			if(element.tagName == "DIV" && !element.hasAttribute("data-panel") && element.firstElementChild.classList.contains("achieveImgHolder")) {
+		Array.from(document.getElementById("personalAchieve").children).forEach((achievementRow) => {
+			if(achievementRow.tagName == "DIV" && !achievementRow.hasAttribute("data-panel") && achievementRow.firstElementChild.classList.contains("achieveImgHolder")) {
+				//順序リストに追加
+				achievementsOrderedList.push(achievementRow.lastElementChild.firstElementChild.firstElementChild.innerText);
 				//ボタンの生成
 				const buttonsDiv = document.createElement("DIV");
 				buttonsDiv.classList.add("sat_buttons");
@@ -36,6 +66,7 @@ if(myNameElement) {
 						goodIcon.classList.add("sat_good_icon_clicked");
 						badIcon.classList.remove("sat_bad_icon_clicked");
 					}
+					sortAchievements();
 				});
 				goodButtonArea.appendChild(goodButton);
 				goodButtonArea.appendChild(goodBallon);
@@ -69,11 +100,12 @@ if(myNameElement) {
 						goodIcon.classList.remove("sat_good_icon_clicked");
 						badIcon.classList.add("sat_bad_icon_clicked");
 					}
+					sortAchievements();
 				});
 				badButtonArea.appendChild(badButton);
 				badButtonArea.appendChild(badBallon);
 				buttonsDiv.appendChild(badButtonArea);
-				element.lastElementChild.appendChild(buttonsDiv);
+				achievementRow.lastElementChild.appendChild(buttonsDiv);
 			}
 		});
 	}
