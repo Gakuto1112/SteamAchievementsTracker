2ccf1e2be65682c4901eef0175679103e6279df2
Gakuto1112 <gakuto131112@gmail.com>
2022-02-07 17:39:04 +0900

    インストール時に設定初期値を追加

---
+++ Event.js
@@ -0,0 +1,11 @@
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.storage.sync.get(null).then((data) => {
+		const userData = data;
+		if(Object.keys(data).length == 0) {
+			userData["settings"] = {};
+			userData["settings"]["mode"] = "sync";
+			chrome.storage.sync.set(userData);
+		}
+		console.log(userData);
+	});
+});
